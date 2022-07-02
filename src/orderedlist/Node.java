5f842a17ef5be52aa67378fea9479b3e016ecc9f
Dipali <dipssonawane123@gmail.com>
2022-07-02 21:28:13 +0530

    Added: data structure program is added

---
+++ src/orderedlist/Node.java
@@ -0,0 +1,12 @@
+package orderedlist;
+
+public class Node<K> {
+    Node next;
+    Integer data;
+
+    public Node(Integer data) {
+        this.next = null;
+        this.data = data;
+        OrdereList.size++;
+    }
+}
